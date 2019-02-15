260aabe0a8c35ebeed80201e28224e7f4635d97a
0xBAMA <jon.baker1222@gmail.com>
2019-02-15 17:33:41 -0500

    little bits

--- includes/voraldo/v.cc
+++ includes/voraldo/v.cc
@@ -42,7 +42,7 @@ void Voraldo_IO::display(std::string filename, double x_rot, double y_rot, doubl
  	const unsigned char black[3] = {0,0,0};
  	const unsigned char pink[3] = {255,0,255};
 
-
+//draw the border for the image
  	//frame top
  	img.draw_line(0,1,image_x_dimension,1,gold);
  	img.draw_line(0,2,image_x_dimension,2,dark_gold);
@@ -60,53 +60,37 @@ void Voraldo_IO::display(std::string filename, double x_rot, double y_rot, doubl
  	img.draw_line(image_x_dimension-2,0,image_x_dimension-2,image_y_dimension,gold);
 
 
- 	int block_xdim = parent->x_dim;
- 	int block_ydim = parent->y_dim;
- 	int block_zdim = parent->z_dim;
-
- 	int block_xdim_squared = block_xdim*block_xdim;
- 	int block_ydim_squared = block_ydim*block_ydim;
- 	int block_zdim_squared = block_zdim*block_zdim;
+ 	int block_xdim = parent->x_dim;                 int block_ydim = parent->y_dim;                 int block_zdim = parent->z_dim;
+ 	int block_xdim_squared = block_xdim*block_xdim; int block_ydim_squared = block_ydim*block_ydim;	int block_zdim_squared = block_zdim*block_zdim;
 
  	vec d_center = vec(block_xdim/2.0,block_ydim/2.0,block_zdim/2.0);
 
+//rotation matricies allowing rotation of the viewer's position
  	mat rotation_x_axis;
  //refernces [column][row]
- 		rotation_x_axis[0][0] = 1;
- 		rotation_x_axis[0][1] = 0;
- 		rotation_x_axis[0][2] = 0;
- 		rotation_x_axis[1][0] = 0;
- 		rotation_x_axis[1][1] = std::cos(x_rot);
- 		rotation_x_axis[1][2] = std::sin(x_rot);
- 		rotation_x_axis[2][0] = 0;
- 		rotation_x_axis[2][1] = -1.0*std::sin(x_rot);
- 		rotation_x_axis[2][2] = std::cos(x_rot);
+ 		rotation_x_axis[0][0] = 1;                       rotation_x_axis[1][0] = 0;                      rotation_x_axis[2][0] = 0;
+ 		rotation_x_axis[0][1] = 0;                       rotation_x_axis[1][1] = std::cos(x_rot);        rotation_x_axis[2][1] = -1.0*std::sin(x_rot);
+ 		rotation_x_axis[0][2] = 0;                       rotation_x_axis[1][2] = std::sin(x_rot);        rotation_x_axis[2][2] = std::cos(x_rot);
 
  	mat rotation_y_axis;
- 		rotation_y_axis[0][0] = std::cos(y_rot);
- 		rotation_y_axis[0][1] = 0;
- 		rotation_y_axis[0][2] = -1.0*std::sin(y_rot);
- 		rotation_y_axis[1][0] = 0;
- 		rotation_y_axis[1][1] = 1;
- 		rotation_y_axis[1][2] = 0;
- 		rotation_y_axis[2][0] = std::sin(y_rot);
- 		rotation_y_axis[2][1] = 0;
- 		rotation_y_axis[2][2] = std::cos(y_rot);
+ 		rotation_y_axis[0][0] = std::cos(y_rot);         rotation_y_axis[1][0] = 0;                      rotation_y_axis[2][0] = std::sin(y_rot);
+ 		rotation_y_axis[0][1] = 0;                       rotation_y_axis[1][1] = 1;                      rotation_y_axis[2][1] = 0;
+ 		rotation_y_axis[0][2] = -1.0*std::sin(y_rot);    rotation_y_axis[1][2] = 0;                      rotation_y_axis[2][2] = std::cos(y_rot);
 
  	mat rotation_z_axis;
- 		rotation_z_axis[0][0] = std::cos(z_rot);
- 		rotation_z_axis[0][1] = std::sin(z_rot);
- 		rotation_z_axis[0][2] = 0;
- 		rotation_z_axis[1][0] = -1.0*std::sin(z_rot);
- 		rotation_z_axis[1][1] = std::cos(z_rot);
- 		rotation_z_axis[1][2] = 0;
- 		rotation_z_axis[2][0] = 0;
- 		rotation_z_axis[2][1] = 0;
- 		rotation_z_axis[2][2] = 1;
+ 		rotation_z_axis[0][0] = std::cos(z_rot);         rotation_z_axis[1][0] = -1.0*std::sin(z_rot);   rotation_z_axis[2][0] = 0;
+ 		rotation_z_axis[0][1] = std::sin(z_rot);         rotation_z_axis[1][1] = std::cos(z_rot);        rotation_z_axis[2][1] = 0;
+ 		rotation_z_axis[0][2] = 0;                       rotation_z_axis[1][2] = 0;                      rotation_z_axis[2][2] = 1;
 
+//the three vectors defining the x,y,z of "display space" i.e. screen space, used for positioning camera in world space
  	vec d_xvec = mul(rotation_x_axis,mul(rotation_y_axis,mul(rotation_z_axis,vec(1,0,0))));
  	vec d_yvec = mul(rotation_x_axis,mul(rotation_y_axis,mul(rotation_z_axis,vec(0,1,0))));
  	vec d_zvec = mul(rotation_x_axis,mul(rotation_y_axis,mul(rotation_z_axis,vec(0,0,1))));
+
+//this is somewhat abstract - starting from the center of the block, define a sphere
+//this sphere has a radius such that the corners of the box are on the surface of the sphere
+//multiplying this radius by some amount >1 will move us out from there, a sphere with the same center
+//the camera is then located somewhere on this sphere, in a position determined by the use of the rotation matricies
 
  	//tip radius is the radius of the sphere that touches the tips of the block's corners
  	double tip_radius = std::sqrt(block_xdim_squared+block_ydim_squared+block_zdim_squared)/2.0;
@@ -115,32 +99,24 @@ void Voraldo_IO::display(std::string filename, double x_rot, double y_rot, doubl
  	//factor of two is for the incremental step of length 0.5
  	//factor of 2.2 is for the tip radius plus the camera sphere radius, 1+1.2
 
- 	vec cam_position = d_center - 1.2*tip_radius*d_yvec;
+ 	vec cam_position = d_center - 1.2*tip_radius*d_yvec; vec cam_up = scale*d_zvec; vec cam_right = scale*d_xvec;
 
- 	vec cam_up = scale*d_zvec; //may need to change the scaling
- 	vec cam_right = scale*d_xvec;
+ 	int image_center_x = (image_x_dimension-1)/2; int image_current_x;
+ 	int image_center_y = (image_y_dimension-1)/2; int image_current_y;
 
-
-
- 	int image_center_x = (image_x_dimension-1)/2;;
- 	int image_center_y = (image_y_dimension-1)/2;;
-
- 	int image_current_x, image_current_y;
-
- 	vec vector_starting_point;
+ 	vec vector_starting_point,vector_test_point;
  	vec vector_increment = 0.5*normalize(-1.0*(cam_position-d_center));
-
- 	vec vector_test_point;
 
  	vec block_min = vec(0,0,0);
  	vec block_max = vec(block_xdim,block_ydim,block_zdim);
 
- 	Vox tempstate;
+ 	Vox temp;
  	RGB	point_color;
+
+  int alpha_sum;
 
   unsigned char image_color[3];
 
- 	bool xtest,ytest,ztest;
  	bool line_box_intersection;
 
  	double t0 = 0;
@@ -153,10 +129,9 @@ void Voraldo_IO::display(std::string filename, double x_rot, double y_rot, doubl
  	for(double x = -(image_x_dimension/2-5); x <= (image_x_dimension/2-5); x++)
  		for(double y = -(image_y_dimension/2-5); y <= (image_y_dimension/2-5); y++)
  		{//init (reset)
- 			line_box_intersection = false;
+ 			line_box_intersection = false; alpha_sum = 0; //reset flag values for the new pixel
 
- 			image_current_x = image_center_x + x;
- 			image_current_y = image_center_y + y;
+ 			image_current_x = image_center_x + x; image_current_y = image_center_y + y; //x and y values for the new pixel
 
  			// if(perspective == true) //this gets added inside the loop - note that the linetest will have to consider the perspective corrected ray
  			// 	vector_increment_perspective = vector_increment + x*0.1*cam_right - y*0.1*cam_up;
@@ -168,38 +143,27 @@ void Voraldo_IO::display(std::string filename, double x_rot, double y_rot, doubl
  			//intersects with the box established by (0,0,0) (x,y,z)
  			//i.e. block_min and block_max
 
- 			//The goal here is to achieve some level of speedup when compared to
- 			//doing an exhaustive check through all points on all vectors from
- 			//the pixels in the image.
+ 			//The goal here is to know whether or not there is data to sample behind any given pixel - this offers a significant speedup
  			line_box_intersection = parent->intersect_ray_bbox(block_min,block_max,vector_starting_point,vector_increment,tmin,tmax,t0,t1);
 
  			if(line_box_intersection)
- 			{//we will need to step through the box
-        //std::cout << "hit" << endl;
+ 			{//the ray hits the box, we will need to step through the box
  				for(double z = tmin; z <= tmax; z+=0.5) //go from close intersection point to far intersection point
  				{
-          //std::cout << "test" << endl;
- 					vector_test_point = vector_starting_point + z*vector_increment;
-          vector_test_point = vec(floor(vector_test_point[0]),floor(vector_test_point[1]),floor(vector_test_point[2]));
- 					tempstate = parent->get_data_by_vector_index(vector_test_point);
+ 					vector_test_point = linalg::floor(vector_starting_point + z*vector_increment);
+ 					temp = parent->get_data_by_vector_index(vector_test_point);
 
- 					if(tempstate.state>0 && tempstate.state<=62)
- 					{
- 						point_color = parent->palette[tempstate.state];
+          alpha_sum += temp.alpha;
 
-            //std::cout << "got point color" << endl;
-
-            image_color[0] = point_color.red;
-            image_color[1] = point_color.green;
-            image_color[2] = point_color.blue;
-
-            //img.draw_point(image_current_x,image_current_y,dark_gold);
-            img.draw_point(image_current_x,image_current_y,image_color);
+          if(alpha_sum >= 255)
+          {
             break;
- 					}
+          }
  				}
+        //the for loop is completed, now process the stack
+
  			}
- 			else
+ 			else //I saw a ray that did not hit the box, I want to paint it black
  			{
  				img.draw_point(image_current_x,image_current_y,black);
  			}
